36a13888938839e1d873568f70b217d6c4dde6c3
Peter Howkins <peter.howkins@marutan.net>
2026-08-19 12:17:48 +0100

    Remove a large number of unused files

--- SF3000/SFlib copy/Plot_Land.h
+++
@@ -1,8 +0,0 @@
-extern long wave_counter;
-extern long wave_counter2;
-extern long wave_counter3;
-
-extern void plot_land_constants(void*);
-extern void fast_rotation(void*,long);
-extern machine_code_land_plot(void);
-extern machine_code_flat_land_plot(void);
--- SF3000/SFlib copy/test_prog.h
+++
@@ -1,7 +0,0 @@
-#include "ship_struct.h"
-
-#include "Misc_Struct.h"
-extern docking_struct docked ;
-
-void new_fighter_control( ship_stack * );
-
